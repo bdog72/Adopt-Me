bb19d8e5d5782c94c720f46f9a34bde450af2e9c
Brian Bycynski <high5bri@hotmail.com>
2018-09-24 05:11:35 -0400

    made a few changes, nothing drastic

--- dist/App.f3ed77da.js
+++ dist/App.f3ed77da.js
@@ -24691,7 +24691,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64617' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64796' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
